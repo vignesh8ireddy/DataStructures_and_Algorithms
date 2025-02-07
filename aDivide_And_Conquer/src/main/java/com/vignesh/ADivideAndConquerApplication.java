6e858e98c69dcca390bf98b15f3641ce64c2533b
Vignesh Reddy Ireddy <144072079+vignesh8reddy@users.noreply.github.com>
2025-02-07 13:20:29 -0600

    Add Divide, Conquer and Combine Paradigm

---
+++ aDivide_And_Conquer/src/main/java/com/vignesh/ADivideAndConquerApplication.java
@@ -0,0 +1,13 @@
+package com.vignesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ADivideAndConquerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ADivideAndConquerApplication.class, args);
+    }
+
+}
